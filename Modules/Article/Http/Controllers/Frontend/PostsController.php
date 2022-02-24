922bff459284d22752ea6907633c7d44804f5ec8
Debasis Roy <roy.debashish@outlook.com>
2022-02-24 22:12:04 +0600

    post page title chaned

--- Modules/Article/Http/Controllers/Frontend/PostsController.php
+++ Modules/Article/Http/Controllers/Frontend/PostsController.php
@@ -11,7 +11,7 @@ class PostsController extends Controller
     public function __construct()
     {
         // Page Title
-        $this->module_title = 'Posts';
+        $this->module_title = 'News';
 
         // module name
         $this->module_name = 'posts';
