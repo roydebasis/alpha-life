7e81b64b131095f72d4f995c3e7de8dac5e083ea
Debasis Roy <roy.debashish@outlook.com>
2022-03-01 22:53:37 +0600

    home page calculator, policy, plans, recent news link fixed

--- Modules/Service/Resources/views/frontend/products/index.blade.php
+++ Modules/Service/Resources/views/frontend/products/index.blade.php
@@ -1,29 +1,38 @@
 @extends('frontend.layouts.app')
 
-@section('title') {{ $content->name }} @endsection
+@section('title') Insurance Plan @endsection
 
 @section('content')
-    <x-page-header pageTitle="{{ $content->name }}"/>
-    <section class="service-section-v3">
+    <x-page-header pageTitle="Insurance Plan"/>
+    <section class="service-section-v3 exclusive-plans">
         <div class="container">
+            <br><br>
+            @if(!$products->isEmpty())
             <div class="row" style="padding-top: 20px; padding-bottom: 20px;">
-                <div class="col-md-12 service-box-container">
-
-                    <div style="border-bottom-right-radius: 50px; border-top-left-radius: 50px; overflow: hidden; background-color: white; border-left: 1px solid #2a2a86; border-right: 1px solid #2a2a86; border-bottom: 1px solid #2a2a86;">
-                        @if($content->featured_image)
-                            <div style="border-top-left-radius: 50px; overflow: hidden;">
-                                <img style="display: block; max-width: 100%; height: auto;" src="{{ url($content->featured_image) }}">
+                @foreach($products as $product)
+                    <div class="col-md-4 service-box-container">
+                        @if($product->featured_image)
+                            <div style="border-top-left-radius: 50px; overflow: hidden; border-left: 1px solid #2a2a86;">
+                                <img style="display: block; max-width: 100%; height: auto;" src="{{ url($product->featured_image) }}">
                             </div>
                         @endif
-                        <div class="service-box">
-                            {!! $content->content !!}
+                        <div
+                            style="border-bottom-right-radius: 50px; overflow: hidden; background-color: white; border-left: 1px solid #2a2a86; border-right: 1px solid #2a2a86;border-bottom: 1px solid #2a2a86;">
+                            <div class="service-box">
+                                <h3 style="text-align: center;"> {{ $product->name }} </h3>
+                                <p style="text-align: justify;"> {{ $product->intro }}, <a
+                                        href="{{ route('frontend.plans.show', ['slug' => $product->slug]) }}"><strong>More...</strong></a></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-
-
+                @endforeach
             </div>
+            <div class="col-12" style="margin-bottom: 50px">
+                <div class="team-pagination">
+                    {!! $products->links() !!}
+                </div>
+            </div>
+            @endif
         </div>
     </section>
 @endsection
--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Modules\Article\Entities\Post;
 use Modules\Page\Entities\Page;
+use Modules\Service\Entities\Service;
 
 class FrontendController extends Controller
 {
@@ -19,6 +20,7 @@ class FrontendController extends Controller
     {
         $body_class = '';
         $blogs = Post::latest()->with(['category', 'tags', 'comments'])->take(3)->get();
+        $insurancePlans = Service::where('product_category_id')
         return view('frontend.index', compact('body_class', 'blogs'));
     }
 
@@ -78,8 +80,13 @@ class FrontendController extends Controller
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function getPage($slug) {
-        $content = Page::where('slug', $slug)->first();
         $meta_page_type = 'page';
+        //redirect to static page
+        if ($slug == 'premium-calculator') {
+            return view('frontend.premium-calculator', compact('meta_page_type'));
+        }
+        $content = Page::where('slug', $slug)->first();
+
         return view('frontend.page', compact('content', 'meta_page_type'));
     }
 }
