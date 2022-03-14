715dcc9f8f0a366313a10835610f57595ac89a66
tushar162011 <mtushar162011@bscse.uiu.ac.bd>
2022-03-15 00:33:32 +0600

    Centered allign copyright message

--- resources/views/auth/footer.blade.php
+++ resources/views/auth/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="py-5 bg-white" id="footer-main">
     <div class="container">
         <div class="row align-items-center justify-content-xl-between">
-            <div class="col-xl-6">
-                <div class="copyright text-center text-xl-left text-muted">
+            <div class="col-xl-12">
+                <div class="copyright text-center text-muted">
                     &copy; {{ config('app.name') }}, {!! setting('footer_text') !!}
                 </div>
             </div>
