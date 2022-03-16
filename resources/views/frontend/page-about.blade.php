ef3a53814105b7daf3d52ed39b466b5a86991cb5
tushar162011 <mtushar162011@bscse.uiu.ac.bd>
2022-03-17 02:20:44 +0600

    Resolved issue

--- resources/views/frontend/page-about.blade.php
+++ resources/views/frontend/page-about.blade.php
@@ -11,37 +11,38 @@
                     <div class="history-cover"></div>
                 </div><!-- /.col-md-6 -->
                 <div class="col-lg-6 col-md-7 no-padding">
-                    <div class="history-wrapper">
-                        @if(isset($aboutAlpha))
-                            <h2>{{ $aboutAlpha->title }} </h2>
+                    @if(isset($aboutAlpha))
+                        <div class="history-wrapper">
+                                <h2>{{ $aboutAlpha->title }} </h2>
 
-                            <div id="historyCarousel" class="carousel slide" data-ride="carousel">
+                                <div id="historyCarousel" class="carousel slide" data-ride="carousel">
 
-                                <!-- Wrapper for slides -->
-                                <div class="carousel-inner" role="listbox">
-                                    <div class="item active">
-                                        <br>
-                                        <div style="border-top-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden; border-left: 1px solid #2a2a86;">
-                                            <img style="display: block; max-width: 100%; height: auto;" src="{{asset($aboutAlpha->image)}}">
+                                    <!-- Wrapper for slides -->
+                                    <div class="carousel-inner" role="listbox">
+                                        <div class="item active">
+                                            <br>
+                                            <div style="border-top-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden; border-left: 1px solid #2a2a86;">
+                                                <img style="display: block; max-width: 100%; height: auto;" src="{{asset($aboutAlpha->image)}}">
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <p> {{ $aboutAlpha->description }}</p>
                                         </div>
                                     </div>
-                                    <div class="item">
-                                        <p> {{ $aboutAlpha->description }}</p>
-                                    </div>
-                                </div>
 
-                                <!-- Controls -->
-                                <a class="left carousel-control" href="#historyCarousel" role="button" data-slide="prev">
-                                    <span class="fa fa-angle-left" aria-hidden="true"></span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                                <a class="right carousel-control" href="#historyCarousel" role="button" data-slide="next">
-                                    <span class="fa fa-angle-right" aria-hidden="true"></span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </div>
-                        @endif    
-                    </div>
+                                    <!-- Controls -->
+                                    <a class="left carousel-control" href="#historyCarousel" role="button" data-slide="prev">
+                                        <span class="fa fa-angle-left" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="right carousel-control" href="#historyCarousel" role="button" data-slide="next">
+                                        <span class="fa fa-angle-right" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                        </div>
+                    @endif    
+
                 </div><!-- /.col-md-4 -->
 
             </div><!-- /.row -->
