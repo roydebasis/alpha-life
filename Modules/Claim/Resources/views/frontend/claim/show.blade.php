462da70f3a378817b0538c9a4c804344ef6cb08a
tushar162011 <mtushar162011@bscse.uiu.ac.bd>
2022-05-25 16:07:13 +0600

    Claim module work done

--- Modules/Claim/Resources/views/frontend/claim/show.blade.php
+++ Modules/Claim/Resources/views/frontend/claim/show.blade.php
@@ -3,27 +3,43 @@
 @section('title') {{ $$module_name_singular->name }} @endsection
 
 @section('content')
-    <x-page-header pageTitle="{{ $$module_name_singular->name }}"/>
-    <section class="service-section-v3">
-        <div class="container">
-            <div class="row" style="padding-top: 20px; padding-bottom: 70px;">
-                <div class="col-md-12 service-box-container">
+    <x-page-header pageTitle="Claim Details"/>
 
-                    <div style="border-bottom-right-radius: 50px; border-top-left-radius: 50px; overflow: hidden; background-color: white; border-left: 1px solid #2a2a86; border-right: 1px solid #2a2a86; border-bottom: 1px solid #2a2a86;">
-                        @if($$module_name_singular->banner_image)
-                            <div style="border-top-left-radius: 50px; overflow: hidden;">
-                                <img style="display: block; max-width: 100%; width: 1200px; height: 500px; margin: 0 auto" src="{{ url($$module_name_singular->banner_image) }}">
-                            </div>
-                        @endif
-                        <div class="service-box">
-                            {!! $$module_name_singular->content !!}
+    <section class="hero-block-v1 section-padding">
+        <div class="container">
+            <div class="row" style="margin-bottom: 20px;">
+                <div class="col-md-12">
+                    <h4>Date:  <span>{{ date('d M, Y', strtotime($$module_name_singular->date)) }}</span></h4>
+                </div>
+            </div>
+            <div class="row" style="margin-bottom: 20px;">
+                <div class="col-md-12">
+                    <h4>Description </h4>
+                    {!! $$module_name_singular->description !!}
+
+                </div>
+            </div>
+            <div class="row" style="margin-bottom: 40px;">
+                <div class="col-md-12">
+                    <h4>Check Image </h4>
+                    <image src="{{ asset($$module_name_singular->check_image) }}" class="img-thumbnail"></image>
+
+                </div>
+            </div>
+            @isset($$module_name_singular)
+                <div class="row" style="margin-bottom: 20px; margin-left: 0;">
+                    <div class="col-md-12">
+                        <h4>Album </h4>
+                        <div class="row" style="display: flex;">
+                            @foreach($$module_name_singular->photos as $photo)
+                                <div class="col-4" style="margin: 10px">
+                                    <image src="{{ asset($photo->url) }}" class="img-thumbnail"></image>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
-
-
-
-            </div>
-        </div>
+            @endisset
+        </div><!-- /.container -->
     </section>
 @endsection
